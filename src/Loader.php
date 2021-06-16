65f810094f8aa53666cbb694137fa834a4fe414d
sebprt <sebastien.parrat@orange.fr>
2021-06-16 10:44:38 +0200

    Removed the log-plugin, added the prefer-stable option in the composer.json and updated dependancies

--- src/Loader.php
+++ src/Loader.php
@@ -5,17 +5,13 @@ namespace Kiboko\Component\Flow\JSON;
 use Kiboko\Component\Bucket\AcceptanceResultBucket;
 use Kiboko\Contract\Pipeline\LoaderInterface;
 use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 
 class Loader implements LoaderInterface
 {
-    private LoggerInterface $logger;
-
     public function __construct(
         private \SplFileObject $file,
         ?LoggerInterface $logger = null
     ) {
-        $this->logger = $logger ?? new NullLogger();
     }
 
     public function load(): \Generator
