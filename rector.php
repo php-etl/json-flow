5df1099d08987e2428e0a1a91cbbe986b78ffb88
sebprt <sebastien@kiboko.fr>
2023-04-18 10:45:47 +0200

    Updated rector rules

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -15,8 +16,9 @@ return static function (RectorConfig $rectorConfig): void {
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
-    // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_82
-    //    ]);
+     // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+    ]);
 };
