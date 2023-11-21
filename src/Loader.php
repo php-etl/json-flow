355665b1481878b472ac9e50f8df60c4d53d4f41
Grégory Planchat <gregory@kiboko.fr>
2023-11-21 16:58:42 +0100

    Updated code to be compatible with php-etl/pipeline-contracts:0.5

--- src/Loader.php
+++ src/Loader.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kiboko\Component\Flow\JSON;
 
 use Kiboko\Component\Bucket\AcceptanceResultBucket;
+use Kiboko\Component\Bucket\EmptyResultBucket;
 use Kiboko\Contract\Pipeline\LoaderInterface;
 
 class Loader implements LoaderInterface
@@ -19,6 +20,10 @@ class Loader implements LoaderInterface
         $line = yield;
 
         while (true) {
+            if ($line === null) {
+                $line = yield new EmptyResultBucket();
+                continue;
+            }
             $this->file->fwrite(json_encode($line, \JSON_THROW_ON_ERROR)."\n");
 
             $line = yield new AcceptanceResultBucket($line);
